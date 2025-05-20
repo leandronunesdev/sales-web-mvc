0411ecca24ea847a58ad6a70ebe5c2a59b452522
Leandro <nunes.pessoal@gmail.com>
2025-05-20 19:47:37 -0300

    simple create form and foreign key not null (referential integrity)

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Services;
-
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -17,6 +17,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }

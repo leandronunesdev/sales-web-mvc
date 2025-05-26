5a04a09db782a31a19ff4c5e7694b5e14ba8644d
Leandro <nunes.pessoal@gmail.com>
2025-05-26 20:38:05 -0300

    implements AddSellerIdToSalesRecord migration

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -4,27 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models.Enums;
 
-namespace SalesWebMvc.Models
+namespace SalesWebMvc.Models;
+
+public class SalesRecord
 {
-    public class SalesRecord
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public double Amount { get; set; }
+    public SaleStatus Status { get; set; }
+
+    public int SellerId { get; set; }
+    public Seller Seller { get; set; }
+
+    public SalesRecord()
     {
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public double Amount { get; set; }
-        public SaleStatus Status { get; set; }
-        public Seller Seller { get; set; }
+    }
 
-        public SalesRecord()
-        {
-        }
-
-        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
-        {
-            Id = id;
-            Date = date;
-            Amount = amount;
-            Status = status;
-            Seller = seller;
-        }
+    public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+    {
+        Id = id;
+        Date = date;
+        Amount = amount;
+        Status = status;
+        Seller = seller;
+        SellerId = seller.Id;
     }
 }
